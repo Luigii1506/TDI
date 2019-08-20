f9c073a537170327ffe9a2d7f01c0b9bffa968d0
luigii1506 <luis.encinas1506@gmail.com>
2019-08-20 10:10:46 -0700

    Se comento la clave de API google maps

--- plugins/vue2-google-maps.js
+++ plugins/vue2-google-maps.js
@@ -3,8 +3,7 @@ import * as VueGoogleMaps from 'vue2-google-maps'
  
 Vue.use(VueGoogleMaps, {
   load: {
-    key: 'AIzaSyCSq9s-cTiAZuC431PuDl0cC0aWOroeH_I',
-
+    //key: 'AIzaSyCSq9s-cTiAZuC431PuDl0cC0aWOroeH_I',
   },
  
   
