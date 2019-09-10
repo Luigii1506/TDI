bd63babe8c4885f02d5c64e03370eaf01b3210b1
luigii1506 <luis.encinas1506@gmail.com>
2019-09-09 19:57:59 -0700

    Nueva forma de lista

--- components/Posts/PostList.vue
+++ components/Posts/PostList.vue
@@ -35,16 +35,16 @@
             >
               
               <v-list-tile-avatar>
-                <img :src="item.avatar">
+                  <v-icon :class="[item.iconClass]" style="font-size: 40px;">{{item.icon}}</v-icon>
               </v-list-tile-avatar>
               <v-list-tile-content>
                 <v-list-tile-title v-html="item.title" style="font-weight: bold;"></v-list-tile-title>
                 <v-list-tile-sub-title v-html="item.subtitle" style="font-size: 12pt; color: gold"></v-list-tile-sub-title>
               </v-list-tile-content>
               <v-list-tile-action>
-              <v-btn icon ripple>
-                <v-icon color="grey lighten-1">info</v-icon>
-              </v-btn>
+               <div :class="[item.numberClas]">
+                 <p class="number-text">{{item.number}}</p>
+              </div>
             </v-list-tile-action>
               
             </v-list-tile>
@@ -69,7 +69,10 @@ export default {
             title: 'PEDWEST',
             subtitle: "(San Diego)",
             id: 0,
-            url: '/posts/0'
+            numberClas: "black white--text number-wrapper",
+            number: 1,
+            icon: '',
+            iconClass: 'white--text'
           },
           { divider: true, inset: true },
           {
@@ -77,7 +80,11 @@ export default {
             title: 'MERCADO DE ARTESANIAS',
             subtitle: "Artisan's Market",
             id: 1,
-            url: '/posts/1'
+            numberClas: "blue white--text number-wrapper",
+            number: 2,
+            icon: 'palette',
+            iconClass: 'white--text'
+
           },
           { divider: true, inset: true },
           {
@@ -85,7 +92,11 @@ export default {
             title: 'MUSEO DE CERA',
             subtitle: "Wax Museum",
             id: 2,
-            url: '/posts/2'
+            numberClas: "red white--text number-wrapper",
+            number: 3,
+            icon: 'bank',
+            iconClass: 'white--text'
+
           },
           { divider: true, inset: true },
           {
@@ -93,7 +104,10 @@ export default {
             title: 'MERCADO DE FLORES',
             subtitle: "Flower Market",
             id: 3,
-            url: '/posts/3'
+            numberClas: "blue white--text number-wrapper",
+            number: 4,
+            icon: 'shopping_cart',
+            iconClass: 'white--text'
           },
           { divider: true, inset: true },
            {
@@ -101,7 +115,10 @@ export default {
             title: 'PLAZA SANTA CECILIA',
             subtitle: "Old Town Tijuana",
             id: 4,
-            url: '/posts/4'
+            numberClas: "green white--text number-wrapper",
+            number:5,
+            icon: '',
+            iconClass: 'white--text'
           },
           { divider: true, inset: true },
           {
@@ -109,7 +126,10 @@ export default {
             title: 'INSTITUTO MUNICIPAL DE ARTE Y CULTURA',
             subtitle: "Arts & Culture Institute",
             id: 5,
-            url: '/posts/5'
+            numberClas: "red white--text number-wrapper",
+            number: 6,
+            icon: 'bank',
+            iconClass: 'white--text'
           },
           { divider: true, inset: true },
           {
@@ -117,7 +137,11 @@ export default {
             title: 'MERCADO TRADICIONES MEXICANAS "EL POP"',
             subtitle: "Crafts & Farmers Market",
             id: 6,
-            url: '/posts/6'
+            numberClas: "blue white--text number-wrapper",
+            number: 7,
+            icon: 'shopping_cart',
+            iconClass: 'white--text'
+
           },
           { divider: true, inset: true },
           {
@@ -125,7 +149,10 @@ export default {
             title: 'CATEDRAL',
             subtitle: "",
             id: 7,
-            url: '/posts/7'
+            numberClas: "red white--text number-wrapper",
+            number: 8,
+            icon: '',
+            iconClass: 'white--text'
           },
           { divider: true, inset: true },
           {
@@ -133,7 +160,10 @@ export default {
             title: 'MERCADO MUNICIPAL',
             subtitle: "Traditional Mexican Food Market",
             id: 8,
-            url: '/posts/8'
+            numberClas: "green white--text number-wrapper",
+            number: 9,
+            icon: 'shopping_cart',
+            iconClass: 'white--text'
           },
           { divider: true, inset: true },
           {
@@ -141,7 +171,10 @@ export default {
             title: 'JAI-ALAI',
             subtitle: "",
             id: 9,
-            url: '/posts/9'
+            numberClas: "red white--text number-wrapper",
+            number: 10,
+            icon: '',
+            iconClass: 'white--text'
           },
           { divider: true, inset: true },
           {
@@ -149,7 +182,10 @@ export default {
             title: 'PARQUE "LA OCHO',
             subtitle: "Park",
             id: 10,
-            url: '/posts/10'
+            numberClas: "red white--text number-wrapper",
+            number: 11,
+            icon: 'place',
+            iconClass: 'white--text'
           },
           { divider: true, inset: true },
           {
@@ -157,7 +193,10 @@ export default {
             title: 'AVE. REVOLUCION',
             subtitle: "Revolution Ave.",
             id: 11,
-            url: '/posts/11'
+            numberClas: "blue darken-4 white--text number-wrapper",
+            number: 'A',
+            icon: 'place',
+            iconClass: 'white--text'
           },
           { divider: true, inset: true },
           {
@@ -165,7 +204,10 @@ export default {
             title: 'CALLE 6A',
             subtitle: "6th Street / Nigthlife Corridor",
             id: 12,
-            url: '/posts/12'
+            numberClas: "green white--text number-wrapper",
+            number: 'B',
+            icon: 'local_bar',
+            iconClass: 'white--text'
          }
         ]
     }
@@ -202,5 +244,20 @@ export default {
   flex-wrap: wrap;
   align-items: center;
   justify-content: center;
+}
+
+.number-wrapper {
+  text-align: center;
+  border-radius: 50%; 
+  width: 40px; 
+  height: 40px;
+}
+
+.number-text {
+  position: relative;
+  top: 50%;
+  transform: translateY(-50%);
+  font-weight: bold;
+  font-size: 27px;
 }
 </style>
