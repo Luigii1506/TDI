ceddd7dd3b6fd7a5ed36e70a9d6942842e345ab4
luigii1506 <luis.encinas1506@gmail.com>
2019-09-09 19:57:41 -0700

    Se cambio el layout

---
+++ layouts/home.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+    <p>Test</p>
+    <v-content class="v-content">
+      <v-container style="padding: 0px; max-width: 100%;">
+        <nuxt />
+      </v-container>
+    </v-content>   
+  </v-app>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style >
+    .v-content {
+        background-image: linear-gradient(to bottom, #004d8a, #005a90, #006693, #007193, #007c93);
+    }
+</style>
