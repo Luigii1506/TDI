3857d0ae6256d3af107cafbb0c15926a95049731
luigii1506 <luis.encinas1506@gmail.com>
2019-09-11 20:14:41 -0700

    Se agrego funcion para llamar

--- pages/posts/_id/index.vue
+++ pages/posts/_id/index.vue
@@ -21,23 +21,32 @@
       <p class="title">{{postItems[id].title}}</p>
       <p class="subtitle">{{postItems[id].subtitle}}</p>
     </div>
+    <hr class="hr-small"/>
     <div class="flags-wrapper">
         <img src="~/assets/flag-america.png" @click="americaFlag" nuxt class="flags">
         <img src="~/assets/china.png" @click="chinaFlag" nuxt class="flags"/>
         <img src="~/assets/mexico.png" @click="mexicoFlag" nuxt class="flags"/>
     </div>
-   <v-carousel hide-delimiters>
+   <v-carousel hide-delimiters class="carrusel">
       <v-carousel-item
         v-for="(item,i) in postItems[id].items"
         :key="i"
         :src="item.src"
       ></v-carousel-item>
     </v-carousel>
+    <div class="phone-wrapper">
+      <v-icon class="iconbottom">place</v-icon>
+       <a :href="postItems[id].phone" style="text-decoration: none;">  
+         <v-icon class="iconbottom">phone</v-icon>
+        </a>
+    </div>
+    <hr class="hr-large"/>
     <div class="info-wrapper">
       <p class="info-text">
         {{ $t('lugaresInfo[' + id + ']') }}
       </p>
     </div>
+    <hr class="hr-large"/>
   </v-container>
 </template>
 
@@ -64,7 +73,8 @@ export default {
                 src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
               },
             ],
-            info: ""
+            info: "",
+            phone: 'tel:664-625-1240',
           },
           {
             title: 'MERCADO DE ARTESANIAS',
@@ -83,7 +93,8 @@ export default {
                 src: 'https://media-cdn.tripadvisor.com/media/photo-s/06/0c/b6/c5/artist-market.jpg',
               },
             ],
-            info: ""
+            info: "",
+            phone: 'tel:664-625-1240'
           },
           {
             title: 'MUSEO DE CERA',
@@ -102,7 +113,8 @@ export default {
                 src: 'https://media-cdn.tripadvisor.com/media/photo-s/0f/db/a3/af/lugar-iconografico-de.jpg',
               },
             ],
-            info: ""
+            info: "",
+            phone: 'tel:664-625-1240'
           },
           {
             title: 'MERCADO DE FLORES',
@@ -121,7 +133,8 @@ export default {
                 src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
               },
             ],
-            info: ""
+            info: "",
+            phone: 'tel:664-625-1240'
           },
           {
             title: 'PLAZA SANTA CECILIA',
@@ -140,7 +153,8 @@ export default {
                 src: 'https://i.pinimg.com/originals/dc/b5/1f/dcb51f054388a843da5de5a7afc1b981.jpg',
               },
             ],
-            info: ""
+            info: "",
+            phone: 'tel:664-625-1240'
           },
           {
             title: 'INSTITUTO MUNICIPAL DE ARTE Y CULTURA',
@@ -159,7 +173,8 @@ export default {
                 src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
               },
             ],
-            info: ""
+            info: "",
+            phone: 'tel:664-625-1240'
           },
           {
             title: 'MERCADO TRADICIONES MEXICANAS "EL POP"',
@@ -178,7 +193,8 @@ export default {
                 src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
               },
             ],
-            info: ""
+            info: "",
+            phone: 'tel:664-625-1240'
           },
           {
             title: 'CATEDRAL',
@@ -197,7 +213,8 @@ export default {
                 src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
               },
             ],
-            info: ""
+            info: "",
+            phone: 'tel:664-625-1240'
           },
           {
             title: 'MERCADO MUNICIPAL',
@@ -216,7 +233,8 @@ export default {
                 src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
               },
             ],
-            info: ""
+            info: "",
+            phone: 'tel:664-625-1240'
           },
           {
             title: 'JAI-ALAI',
@@ -235,7 +253,8 @@ export default {
                 src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
               },
             ],
-            info: ""
+            info: "",
+            phone: 'tel:664-625-1240'
           },
           {
             title: 'PARQUE "LA OCHO"',
@@ -254,7 +273,8 @@ export default {
                 src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
               },
             ],
-            info: ""
+            info: "",
+            phone: 'tel:664-625-1240'
           },
           {
             title: 'AVE. REVOLUCION',
@@ -273,7 +293,8 @@ export default {
                 src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
               },
             ],
-            info: ""
+            info: "",
+            phone: 'tel:664-625-1240'
           },
           {
             title: 'CALLE 6A',
@@ -292,7 +313,8 @@ export default {
                 src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
               },
             ],
-            info: ""
+            info: "",
+            phone: 'tel:664-625-1240'
           }
         ]
       }
@@ -322,12 +344,51 @@ export default {
 
 <style scoped>
 
+.iconbottom {
+    color: white;
+    background-color: goldenrod;
+    font-size: 40px;
+    border-radius: 8px;
+    padding: 5px;
+    margin-left: 10px;
+    margin-right: 5px;
+}
+
+.carrusel {
+  margin-bottom: 10px;
+}
+
+.hr-small {
+  border:0;
+  margin: auto;
+  width: 50%;
+  height: 4px;
+  background:goldenrod;
+}
+
+.hr-large {
+  border:0;
+  margin:0;
+  width:100%;
+  height: 3px;
+  background:goldenrod;
+}
+
 .flags-wrapper {
   width: max-content;
   margin-left: auto;
   position: relative;
   z-index: 9999;
   margin-top: 25px;
+}
+
+.phone-wrapper {
+  width: max-content;
+  margin-left: auto;
+  position: relative;
+  z-index: 9999;
+  margin-bottom: 12px;
+  margin-top: 15px;
 }
 
 .flags {
@@ -337,12 +398,15 @@ export default {
 }
 
 .container-padding-bottom {
+  padding-left: 0;
+  padding-right: 0;
+  padding-top: 0;
   padding-bottom: 60px;
 }
 
 .info-wrapper {
-  margin-top: 30px;
-  margin-bottom: 30px;
+  margin-top: 15px;
+  margin-bottom: 15px;
 }
 
 .info-text {
@@ -352,8 +416,8 @@ export default {
 }
 
 .title-wrapper {
-  margin-top: 30px;
-  margin-bottom: 30px;
+  margin-top: 15px;
+  margin-bottom: 5px;
 }
 
 .title {
@@ -383,10 +447,6 @@ export default {
   margin-top: 35px;
   font-size: 18px;
   text-align: left;
-}
-
-.carrusel {
-  margin-top: 30px;
 }
 
 .single-post-page {
