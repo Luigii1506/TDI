d60b897ff58c26d860b9b1e86be24b91e8fece11
luigii1506 <luis.encinas1506@gmail.com>
2019-09-12 06:45:03 -0700

    Se activo la llave de google maps

--- plugins/vue2-google-maps.js
+++ plugins/vue2-google-maps.js
@@ -3,6 +3,6 @@ import * as VueGoogleMaps from 'vue2-google-maps'
  
 Vue.use(VueGoogleMaps, {
   load: {
-   // key: 'AIzaSyCSq9s-cTiAZuC431PuDl0cC0aWOroeH_I',
+    key: 'AIzaSyCSq9s-cTiAZuC431PuDl0cC0aWOroeH_I',
   },
 })
