c49a4f596ce97b7bc6e65f54f8c4e1fbf61a9c69
luigii1506 <luis.encinas1506@gmail.com>
2019-10-03 18:39:37 -0700

    Se agrego prueba para conectarse a internet a traves del app

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,8 +1,38 @@
 <template>
 
-<p>Este es el about de la pagina</p>
+<v-form method="post" action="https://securelogin.arubanetworks.com/cgi-bin/login" id="nativeForm" v-model="valid">
+
+      <v-btn @click="submit" :disabled="!valid">submit</v-btn>
+ </v-form>
 
 </template>
+
+ <script>
+    export default {
+      name: 'contact',
+
+      data () {
+         return { 
+            valid: false,
+            name: '',
+            nameRules: [
+              (v) => !!v || 'Name is required',
+              (v) => v.length <= 10 || 'Name must be less than 10 characters'
+            ],
+            email: '',
+            emailRules: [
+              (v) => !!v || 'E-mail is required',
+              (v) => /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'E-mail must be valid'
+            ]
+          }
+        },
+        methods: {
+          submit() {
+            nativeForm.submit()
+          }
+        }
+      }
+  </script>
 
 <style scoped>
 
