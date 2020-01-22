139c63590d193ba7844becea054779c9ddc9df29
Luis Gerardo <luis.encinas1506@gmail.com>
2020-01-21 17:47:03 -0800

    Se agrego comentarios puro css y html

--- pages/posts/_id/index.vue
+++ pages/posts/_id/index.vue
@@ -58,10 +58,33 @@
       <p class="dia"><span>{{ $t('sabado') }}:</span><span class="hora">{{postItems[id].horarios[5]}}</span></p>
       <p class="dia"><span>{{ $t('domingo') }}:</span><span class="hora">{{postItems[id].horarios[6]}}</span></p>
     </div>
+     <div class="comments-outside">
+        <div class="comments-header">
+          <div class="comments-stats">
+              <span><i class="fa fa-thumbs-up"></i> {{ likes }}</span>
+              <span><i class="fa fa-comment"></i> {{ comments.length }}</span>
+            </div>
+            <div class="post-owner">
+              <div class="avatar">
+                <img :src="creator.avatar" alt="">
+              </div>
+              <div class="username">
+                <a href="#">@{{ creator.user }}</a>
+              </div>
+            </div>
+          </div>
+          <comments 
+            :comments_wrapper_classes="['custom-scrollbar', 'comments-wrapper']"
+            :comments="comments"
+            :current_user="current_user"
+            @submit-comment="submitComment"
+          ></comments>
+      </div>
   </v-container>
 </template>
 
 <script>
+import Comments from '@/components/Comments/Comments.vue'
 
 export default {
     data () {
@@ -281,6 +304,35 @@ export default {
             phone: '',
             horarios: ['24 horas','24 horas','24 horas','24 horas','24 horas','24 horas','24 horas']
           }
+        ],
+        likes: 15,
+        creator: {
+          avatar: 'http://via.placeholder.com/100x100/a74848',
+          user: 'exampleCreator'
+        },
+        current_user: {
+          avatar: 'http://via.placeholder.com/100x100/a74848',
+          user: 'exampler'
+        },
+        comments: [
+          {
+            id: 1,
+            user: 'example',
+            avatar: 'http://via.placeholder.com/100x100/a74848',
+            text: 'lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor lorem ipsum dolor ',
+          },
+          {
+            id: 2,                            
+            user: 'example1',
+            avatar: 'http://via.placeholder.com/100x100/2d58a7',
+            text: 'lorem ipsum dolor',
+          },
+          {
+            id: 3,                            
+            user: 'example2',
+            avatar: 'http://via.placeholder.com/100x100/36846e',
+            text: 'lorem ipsum dolor again',
+          },
         ]
       }
     },
@@ -301,6 +353,14 @@ export default {
     chinaFlag() {
       this.dialog = false;
       this.$router.push(this.switchLocalePath('ch'));
+    },
+    submitComment(reply) {
+      this.comments.push({
+        id: this.comments.length + 1,
+        user: this.current_user.user,
+        avatar: this.current_user.avatar,
+        text: reply
+      });
     }
   },
   mounted() {
@@ -311,6 +371,9 @@ export default {
     } else {
       this.bandera = 'US'
     }
+  },
+  components: {
+    Comments
   }
 };
 </script>
@@ -460,6 +523,63 @@ export default {
 .post {
   width: 100%;
 }
+
+/* Comments */
+
+a {
+  text-decoration: none;
+}
+
+hr {
+  display: block;
+  height: 1px;
+  border: 0;
+  border-top: 1px solid #ececec;
+  margin: 1em 0;
+  padding: 0;
+}
+
+.comments-outside {
+  box-shadow: 2px 2px 8px rgba(0, 0, 0, 0.3);
+  margin: 0 auto;
+  max-width: 600px;
+}
+
+.comments-header {
+  background-color: #C8C8C8;
+  padding: 10px;
+  align-items: center;
+  display: flex;
+  justify-content: space-between;
+  color: #333;
+  min-height: 80px;
+  font-size: 20px;
+}
+
+.comments-header .comments-stats span {
+  margin-left: 10px;
+}
+
+.post-owner {
+  display: flex;
+  align-items: center;
+}
+
+.post-owner .avatar > img {
+  width: 30px;
+  height: 30px;
+  border-radius: 100%;
+}
+
+.post-owner .username {
+  margin-left: 5px;
+}
+
+.post-owner .username > a {
+  color: #333;
+}
+
+/* End Comments */
 
 @media (min-width: 768px) {
   .post {
