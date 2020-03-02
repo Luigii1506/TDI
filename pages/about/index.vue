7faa1217558430bb77ac2fb0c9a87d6a052d42b6
Luis Gerardo Encinas Ramos <luis.encinas1506@gmail.com>
2020-03-02 13:23:25 -0800

    prueba de wifi auth

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,7 +1,24 @@
 <template>
 
-<v-form method="post" action="https://securelogin.arubanetworks.com/cgi-bin/login" id="nativeForm" v-model="valid">
+<!--
+ <v-form method="post" action="https://securelogin.arubanetworks.com/cgi-bin/login" id="nativeForm" v-model="valid">
+      <v-text-field v-model="name" label="Nombres"></v-text-field>
+      <v-btn @click="submit" :disabled="!valid">submit</v-btn>
+ </v-form>
+ https://securelogin.hpe.com/cgi-bin/login
 
+-->
+
+
+<v-form method="post" id="nativeForm" v-model="valid">
+      <!--
+      <input type="hidden" id="hidden1" value="Guest_Authenticated">  
+      <v-text-field v-model="name" label="Nombres"></v-text-field>
+      -->
+      <input type="hidden" id="hidden1" value="Guest_Authenticated" v-if="awesome"> 
+      <v-text-field v-model="name" label="Tu primer nombre"></v-text-field>
+      <v-text-field v-model="name2" label="Te segundo nombre"></v-text-field>
+      <v-text-field v-model="correo" label="Te correo"></v-text-field>
       <v-btn @click="submit" :disabled="!valid">submit</v-btn>
  </v-form>
 
@@ -9,26 +26,24 @@
 
  <script>
     export default {
-      name: 'contact',
-
       data () {
          return { 
             valid: false,
-            name: '',
-            nameRules: [
-              (v) => !!v || 'Name is required',
-              (v) => v.length <= 10 || 'Name must be less than 10 characters'
-            ],
-            email: '',
-            emailRules: [
-              (v) => !!v || 'E-mail is required',
-              (v) => /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'E-mail must be valid'
-            ]
+              name: '',
+              name2: '',
+              correo: '',
+              awesome: false
           }
         },
         methods: {
           submit() {
-            nativeForm.submit()
+            if(this.name !== '' && this.name2 !== '' && this.correo !== '') {
+              this.awesome = true
+            }
+            console.log('Presionando boton', this.name);
+            console.log('Presionando boton', this.name2);
+            console.log('Presionando boton', this.correo);
+            //nativeForm.submit()
           }
         }
       }
---
+++ pages/guest/index.vue
@@ -0,0 +1,10 @@
+<template>
+<div>
+    <p>Este es la pagina de guest</p>
+     <h2>You are authenticated. Enjoy browsing at Arubanetworks</h2>  
+    <form>  
+        <v-text-field></v-text-field>  
+    </form>  
+</div>
+    
+</template>
